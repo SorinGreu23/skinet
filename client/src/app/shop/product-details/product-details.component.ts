23855f063c228a1dfcd0c5b9222fe2254593b2b4
Sorin Greu <greusorin2003@gmail.com>
2020-08-31 12:56:51 +0300

    End of section 14

--- client/src/app/shop/product-details/product-details.component.ts
+++ client/src/app/shop/product-details/product-details.component.ts
@@ -3,6 +3,7 @@ import { IProduct } from 'src/app/shared/models/product';
 import { ShopService } from '../shop.service';
 import { ActivatedRoute } from '@angular/router';
 import { BreadcrumbService } from 'xng-breadcrumb';
+import { BasketService } from 'src/app/basket/basket.service';
 
 @Component({
   selector: 'app-product-details',
@@ -11,8 +12,14 @@ import { BreadcrumbService } from 'xng-breadcrumb';
 })
 export class ProductDetailsComponent implements OnInit {
   product: IProduct;
+  quantity = 1;
 
-  constructor(private shopService: ShopService, private activateRoute: ActivatedRoute, private bcService: BreadcrumbService) {
+  constructor(
+    private shopService: ShopService,
+    private activateRoute: ActivatedRoute,
+    private bcService: BreadcrumbService,
+    private basketService: BasketService
+  ) {
     this.bcService.set('@productDetails', '');
   }
 
@@ -21,15 +28,34 @@ export class ProductDetailsComponent implements OnInit {
   }
 
   // tslint:disable-next-line: typedef
+  addItemToCart() {
+    this.basketService.addItemToBasket(this.product, this.quantity);
+  }
+
+  // tslint:disable-next-line: typedef
+  incrementQuantity() {
+    this.quantity++;
+  }
+
+  // tslint:disable-next-line: typedef
+  decrementQuantity() {
+    if (this.quantity > 1) {
+      this.quantity--;
+    }
+  }
+
+  // tslint:disable-next-line: typedef
   loadProduct() {
-    this.shopService.getProduct(+this.activateRoute.snapshot.paramMap.get('id')).subscribe(
-      (product) => {
-        this.product = product;
-        this.bcService.set('@productDetails', product.name);
-      },
-      (error) => {
-        console.log(error);
-      }
-    );
+    this.shopService
+      .getProduct(+this.activateRoute.snapshot.paramMap.get('id'))
+      .subscribe(
+        (product) => {
+          this.product = product;
+          this.bcService.set('@productDetails', product.name);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
   }
 }
